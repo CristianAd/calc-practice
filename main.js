316614cacd66f605fadeeeb93510b8bc844058c7
CristianAd <sonlatin2@gmail.com>
2023-10-28 22:39:53 -0500

    sondeo funcionalidad

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 const buttonZone = document.getElementById("buttonZone");
+const screenZone = document.getElementById("screenZone");
 
 function construir() {
   for (let i = 0; i <= 9; i++) {
     const calcBtn = document.createElement("div");
-    calcBtn.textContent = i; // Establece el contenido de texto en el div
+    calcBtn.textContent = i; 
     buttonZone.appendChild(calcBtn);
   }
 }
@@ -23,12 +24,48 @@ function crearOperadoresBtn() {
 }
 
 crearOperadoresBtn();
-construir(); // Llamar construir() después de crear operadores y números
+construir(); 
 
 const operacionMath = document.querySelectorAll("#buttonZone span, #buttonZone div");
 
 operacionMath.forEach((elemento) => {
   elemento.addEventListener("click", () => {
-    console.log(elemento.textContent);
+   // console.log(elemento.textContent);
+        const botonPresionado = elemento.textContent
+        const defaultValorNum = parseInt(screenZone.innerHTML)
+         
+          switch (botonPresionado  ) {
+              case "+":
+                screenZone.innerHTML = 0
+                console.log("es mas")
+              break;
+              case "-":
+                screenZone.innerHTML = 0
+                console.log("es menos")
+              break; 
+                case "*":
+                  screenZone.innerHTML = 0
+                  console.log("es multiplicacion")
+              break; 
+                case "/":
+                  screenZone.innerHTML = 0
+                    console.log("es division")
+                  break; 
+                  case "1":
+                    screenZone.innerHTML = 1
+                      console.log("es division")
+                    break;
+                    case "2":
+                      screenZone.innerHTML = 2
+                        console.log("es division")
+                      break; 
+          
+            default:
+              break;
+          }
+     
   });
 });
+
+
+
